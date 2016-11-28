aa25bd3ec74b90ba01a62249db42748fba306042
Marc Tifrea <marc.tifrea@gmail.com>
2016-11-28 11:51:19 -0600

    Processes only existing particles instead of every position (better performance)

--- src/Display.java
+++ src/Display.java
@@ -1,4 +1,3 @@
-
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
@@ -19,185 +18,190 @@ import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 public class Display extends JFrame {
-	static int count = 0;
-	public static int xVal;
-	public static int numLevels = 10;
-	public static int yVal;
-	public static int xVal2;
-	public static int yVal2;
-	public static int tempXD, tempYD, tempXE, tempYE;
-	public static int xA, xB, xC, xD;
-	public static int yA, yB, yC, yD;
-	public static int xAB, yAB;
-	public static int yE, xE;
+    static int count = 0;
+    public static int xVal;
+    public static int numLevels = 10;
+    public static int yVal;
+    public static int xVal2;
+    public static int yVal2;
+    public static int tempXD, tempYD, tempXE, tempYE;
+    public static int xA, xB, xC, xD;
+    public static int yA, yB, yC, yD;
+    public static int xAB, yAB;
+    public static int yE, xE;
 
-	private static final long serialVersionUID = 1L;
-        
-        
-	public static void main(String[] args) {
+    private static final long serialVersionUID = 1L;
 
-		// creates and shows the gui
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				createAndShowGUI();
 
-			}
-		});
+    public static void main(String[] args) {
 
-	}
+        // creates and shows the gui
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
 
-	public static void createAndShowGUI() {
+            }
+        });
 
-		// The inner workings of creating the specific window of the GUI
+    }
 
-		System.out.println("Created GUI on EDT? "
-				+ SwingUtilities.isEventDispatchThread());
+    public static void createAndShowGUI() {
 
-		// create a JFrame object, the window will be called square example.
-		// tutorial for making a menu via oracle's website:
-		// https://docs.oracle.com/javase/tutorial/uiswing/components/menu.html
-		JMenuBar menuBar = new JMenuBar();
-		JMenu menu = new JMenu("file");
-		menuBar.add(menu);
-		JMenuItem menuItem2 = new JMenuItem("Quit");
-		menu.add(menuItem2);
+        // The inner workings of creating the specific window of the GUI
 
-		// https://stackoverflow.com/questions/9778621/how-to-make-a-jmenu-item-do-something-when-its-clicked
-		// for making a menu do something when clicked
+        System.out.println("Created GUI on EDT? "
+                + SwingUtilities.isEventDispatchThread());
 
-		menuItem2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				System.exit(0);
+        // create a JFrame object, the window will be called square example.
+        // tutorial for making a menu via oracle's website:
+        // https://docs.oracle.com/javase/tutorial/uiswing/components/menu.html
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("file");
+        menuBar.add(menu);
+        JMenuItem menuItem2 = new JMenuItem("Quit");
+        menu.add(menuItem2);
 
-			}
+        // https://stackoverflow.com/questions/9778621/how-to-make-a-jmenu-item-do-something-when-its-clicked
+        // for making a menu do something when clicked
 
-		});
+        menuItem2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
 
-		JFrame frame = new JFrame("Sand Falling Game");
-		frame.setJMenuBar(menuBar);
-                  
-		// When we close out of the application, we will exit it using this
-		// line.
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            }
 
-		// display the window
+        });
 
-		final sqFrame sm = new sqFrame();
-	
-							
-						
-		sm.repaint();
+        JFrame frame = new JFrame("Sand Falling Game");
+        frame.setJMenuBar(menuBar);
 
-		frame.add(sm);
-                
-                //Timer for repetitive updating
-                int timeSlice = 200;  // updates the every assigned number of milliseconds
-                Timer timer = new Timer(timeSlice,  (e) -> sm.repaint());
-                
-		// use pack to size the window appropriately (or can be manually set).
-		frame.pack();
+        // When we close out of the application, we will exit it using this
+        // line.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// display the window
-		frame.setVisible(true);
-                
-                timer.start();
-	}
-       
+        // display the window
+
+        final sqFrame sm = new sqFrame();
+
+
+
+        sm.repaint();
+
+        frame.add(sm);
+
+        //Timer for repetitive updating
+        int timeSlice = 50;  // updates the every assigned number of milliseconds
+        Timer timer = new Timer(timeSlice,  (e) -> sm.repaint());
+
+        // use pack to size the window appropriately (or can be manually set).
+        frame.pack();
+
+        // display the window
+        frame.setVisible(true);
+
+        timer.start();
+    }
+
 }
 
 //Reads mouse input and paints user-display for the sand box
 class sqFrame extends Canvas implements MouseListener{
-	private static final long serialVersionUID = 1L;
-	int xWindow = 1240;
-	int yWindow = 800;
-	int[] xSPoints = new int[4];
-	int[] ySPoints = new int[4];
-	int[] tXPoints = new int[3];
-	int[] tYPoints = new int[3];
-       
-        
-        //game logic object
-        GameWorld gameWorld = new GameWorld();
-        
-        //Mouse-drag tracking variables
-        boolean mouseBeenDragging;//True while the user is holding the mouse down with drag-mode on
-        int startX, startY;//(x,y) coordinates of the point at which the user started holding the mouse down
-        
-        
-        public sqFrame(){
-            addMouseListener(this);
+    private static final long serialVersionUID = 1L;
+    int xWindow = 1240;
+    int yWindow = 800;
+    int[] xSPoints = new int[4];
+    int[] ySPoints = new int[4];
+    int[] tXPoints = new int[3];
+    int[] tYPoints = new int[3];
+
+
+    //game logic object
+    GameWorld gameWorld = new GameWorld();
+
+    //Mouse-drag tracking variables
+    boolean mouseBeenDragging;//True while the user is holding the mouse down with drag-mode on
+    int startX, startY;//(x,y) coordinates of the point at which the user started holding the mouse down
+
+
+    public sqFrame(){
+        addMouseListener(this);
+        mouseBeenDragging = false;
+        startX = -1;
+        startY = -1;
+    }
+
+    // generate the size of the window
+    public Dimension getPreferredSize()
+    {
+        return new Dimension(xWindow, yWindow);
+    }
+
+    public void paint(Graphics gfx){
+
+        //Updates each pixel on-screen, bottom to top, in horizontal layers
+
+        /*for(int iY = gameWorld.PIXEL_MAP_HEIGHT-1; iY >= 0; iY--)
+        {
+            for(int iX = 0; iX < gameWorld.PIXEL_MAP_WIDTH; iX++)
+            {
+                drawPixel(gameWorld.pixelMap[iX][iY], gfx);
+            }
+        }*/
+        for(Particle p : gameWorld.existingParticles)
+        {
+            drawPixel(p,gfx);
+        }
+
+
+        //Updates game variables
+        gameWorld.update();
+    }
+
+
+
+    //Draws pixel of the given type in the alloted pixelMap coordinate
+    public void drawPixel(Particle thisParticle, Graphics gfx)
+    {
+        //Determines pixel color
+        gfx.setColor(thisParticle.color);
+        gfx.fillRect(thisParticle.x*4, thisParticle.y*4, 4, 4);
+        gfx.setColor(Color.black);
+    }
+
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    public void mousePressed(MouseEvent e)
+    {
+        if(gameWorld.penDragMode)
+        {
+            mouseBeenDragging = true;
+            Point pos = e.getPoint();
+            startX = pos.x;
+            startY = pos.y;
+        }
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        if(mouseBeenDragging)
+        {
             mouseBeenDragging = false;
-            startX = -1;
-            startY = -1;
-        }
-        
-	// generate the size of the window
-	public Dimension getPreferredSize() 
+            Point pos = e.getPoint();
+            gameWorld.mouseDragged(startX/4, startY/4, pos.x/4, pos.y/4);
+        } else if(!gameWorld.penDragMode)
         {
-		return new Dimension(xWindow, yWindow);
-	}
-        
-        public void paint(Graphics gfx){
-            
-            //Updates each pixel on-screen, bottom to top, in horizontal layers
-            for(int iY = gameWorld.PIXEL_MAP_HEIGHT-1; iY >= 0; iY--)
-            {
-                for(int iX = 0; iX < gameWorld.PIXEL_MAP_WIDTH; iX++)
-                {
-                    drawPixel(gameWorld.pixelMap[iX][iY], gfx);
-                }
-            }
-            //Updates game variables
-           gameWorld.update();
+            Point pos = e.getPoint();
+            gameWorld.mouseClicked(pos.x/4, pos.y/4);
+            System.out.println("Clicked: " + pos.x/4 + " " + pos.y/4);
+            System.out.println(gameWorld.pixelMap[pos.x/4][pos.y/4].name);
         }
+    }
 
-        
-       
-        //Draws pixel of the given type in the alloted pixelMap coordinate
-        public void drawPixel(Particle thisParticle, Graphics gfx)
-        {
-            //Determines pixel color
-            gfx.setColor(thisParticle.color);
-            gfx.fillRect(thisParticle.x*4, thisParticle.y*4, 4, 4);
-            gfx.setColor(Color.black);
-        }
-      
-        public void mouseClicked(MouseEvent e) {
-            
-        }
+    public void mouseEntered(MouseEvent e) {
+    }
 
-        public void mousePressed(MouseEvent e) 
-        {
-            if(gameWorld.penDragMode)
-            {
-                mouseBeenDragging = true;
-                Point pos = e.getPoint();
-                startX = pos.x;
-                startY = pos.y;
-            }
-        }
-
-        public void mouseReleased(MouseEvent e) {
-            if(mouseBeenDragging)
-            {
-                mouseBeenDragging = false;
-                Point pos = e.getPoint();
-                gameWorld.mouseDragged(startX/4, startY/4, pos.x/4, pos.y/4);
-            } else if(!gameWorld.penDragMode)
-            {
-                Point pos = e.getPoint();
-                gameWorld.mouseClicked(pos.x/4, pos.y/4);
-                System.out.println("Clicked: " + pos.x/4 + " " + pos.y/4);
-                System.out.println(gameWorld.pixelMap[pos.x/4][pos.y/4].name);
-            }
-        }
-
-        public void mouseEntered(MouseEvent e) {
-        }
-
-        public void mouseExited(MouseEvent e) {
-        }
+    public void mouseExited(MouseEvent e) {
+    }
 }
-
-
 
--- src/GameWorld.java
+++ src/GameWorld.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 import java.util.Random;
+import java.util.*;
+
 
 //TODO: Add more pixelTypes. Update ELEMENT_COUNT
 
@@ -9,74 +11,85 @@ public class GameWorld
     //Map of all the pixels on-screen
     //Send this to user interface component to update user-display
     //2Darray indices represent (x,y) coordinates of each pixels
+    ArrayList<Particle> existingParticles;
     final int PIXEL_MAP_HEIGHT = 800/4;
     final int PIXEL_MAP_WIDTH = 1240/4;
     Particle[][] pixelMap;//(0,0) coordinates are located at the top-left of the 2D array
-    
+
     //General-use random variable
     Random randy = new Random();
-    
+
     //Associates an element name with an array index.
     //Determines name of particle on pixelMap where
     //pixelMap's stored integer represents an index in the pixelType array
     final int ELEMENT_COUNT = 3;
     String[] pixelNameList = new String[ELEMENT_COUNT];
-    
+
     //Boolean variables to keep track of mouse operations by user
     boolean mouseWasClicked;//Turns "true" when the visual component indicates the mouse was clicked
     int clickedX, clickedY;//Stores x,y coordinates of the last point the mouse was clicked
     boolean mouseWasDragged;//Turns "true" when the visual component indicates the mouse was dragged
     int draggedX1, draggedX2, draggedY1, draggedY2;//x,y coordinates of dragging mouse's the starting point and ending point
-    
+
     //Variables to keep track of custom options
     String penElement;
     boolean penDragMode;
     boolean wallCollision;
-    
+
     //Default constructor
     public GameWorld()
     {
         //Initializes pixelMap with "Nothing" in all indices
         pixelMap = new Particle[PIXEL_MAP_WIDTH][PIXEL_MAP_HEIGHT];
+        existingParticles = new ArrayList<Particle>();
         for(int iY = PIXEL_MAP_HEIGHT-1; iY >= 0; iY--){
             for(int iX = 0; iX < PIXEL_MAP_WIDTH; iX++){
                 pixelMap[iX][iY] = new Particle("Nothing", Color.black, true, iX, iY);
             }
         }
-        
+
         //Initializes pixelType with every element to be used in-game
         pixelNameList[0] = "Nothing";
         pixelNameList[1] = "Sand";//Completely unreactive non-static particle
         pixelNameList[2] = "Wall";//Completely unreactive static paticle
-        
+
         //Initializes mouse variables
         mouseWasClicked = false;
         mouseWasDragged = false;
-        
+
         //Initializes toggled option variables
-        penElement = "Wall";
+        penElement = "Sand";
         penDragMode = false;
         wallCollision = false;
     }
-    
+
     //Assesses the situation and determines the next course of action for the game
     //We must find a way to call this repeatedly during gamepay
     public void update(){
-        
+
         //Updates each pixel on-screen, bottom to top, in horizontal layers
-        for(int iY = PIXEL_MAP_HEIGHT-1; iY >= 0; iY--){
+
+        /*for(int iY = PIXEL_MAP_HEIGHT-1; iY >= 0; iY--){
             for(int iX = 0; iX < PIXEL_MAP_WIDTH; iX++){
-                
+
                 updatePixel(pixelMap[iX][iY]);
-                
+
                 //Removes any pixels at the screen edge if wall-collision is off
                 if(!wallCollision && (iX == 0 || iY == 0 || iX == PIXEL_MAP_WIDTH-1 || iY == PIXEL_MAP_HEIGHT-1))
                 {
                     pixelMap[iX][iY] = new Particle(iX,iY);
                 }
             }
+        }*/
+        for(Particle p : existingParticles)
+        {
+            updatePixel(p);
+            if(!wallCollision && (p.x == 0 || p.y == 0 || p.x == PIXEL_MAP_WIDTH-1 || p.y == PIXEL_MAP_HEIGHT-1))
+            {
+                pixelMap[p.x][p.y] = new Particle(p.x,p.y);
+            }
         }
-        
+
         //Handles response to a mouse-click
         if(mouseWasClicked)
         {
@@ -86,27 +99,36 @@ public class GameWorld
                     //If there's something in the target spot, replace it with nothing
                     if(!pixelMap[clickedX][clickedY].name.equals("Nothing"))
                     {
-                        pixelMap[clickedX][clickedY] = new Particle(clickedX, clickedY);
+                        Particle p = new Particle(clickedX, clickedY);
+                        pixelMap[clickedX][clickedY] = p;
+                        // existingParticles.add(p);
+
+
                     }
                     break;
                 case "Sand":
                     //If there's nothing in the target spot, replace it with sand
                     if(pixelMap[clickedX][clickedY].name.equals("Nothing"))
                     {
-                        pixelMap[clickedX][clickedY] = new Particle("Sand", Color.lightGray, false, clickedX, clickedY);
+                        Particle p =  new Particle("Sand", Color.lightGray, false, clickedX, clickedY);
+                        pixelMap[clickedX][clickedY] = p;
+                        existingParticles.add(p);
                     }
                     break;
                 case "Wall":
                     //If there's nothing in the target spot, replace it with wall
                     if(pixelMap[clickedX][clickedY].name.equals("Nothing"))
                     {
-                        pixelMap[clickedX][clickedY] = new Particle("Wall", Color.darkGray, true, clickedX, clickedY);
+                        Particle p =  new Particle("Wall", Color.darkGray, true, clickedX, clickedY);
+                        pixelMap[clickedX][clickedY] = p;
+                        existingParticles.add(p);
+                        
                     }
                     break;
             }
         }
     }
-    
+
     //Updates the pixel space with the given particle
     //only considers pixel spaces below the given particle
     public void updatePixel(Particle thisPixel){
@@ -114,7 +136,7 @@ public class GameWorld
         if(thisPixel.name.equals("Nothing"))
         {
             return;
-        } 
+        }
         //Change nothing if particle doesn't fall
         else if(thisPixel.isStatic)
         {
@@ -189,20 +211,20 @@ public class GameWorld
                     particleInteract(pixelMap[thisPixel.x+1][thisPixel.y+1], pixelMap[thisPixel.x][thisPixel.y]);
                 }
             }
-            
+
             //If there's no empty space and no reactive elements underneath, no changes should be made to the particle in-question
         }
-        
-        
-        
+
+
+
     }
-    
+
     //Updates pixelMap after particleA and particleB interact with each other
     public void particleInteract(Particle particleA, Particle particleB)
     {
-        
+
     }
-    
+
     //Returns true if particleA and particleB can interact with each other
     public boolean canParticleInteract(Particle particleA, Particle particleB)
     {
@@ -213,7 +235,7 @@ public class GameWorld
         */
         return false;
     }
-    
+
     //Called by Display when the mouse is clicked at a specific location on the pixel map
     //parameters accept the pixelMap coordinate at which the mouse was clicked
     public void mouseClicked(int x, int y){
@@ -221,7 +243,7 @@ public class GameWorld
         clickedY = y;
         mouseWasClicked = true;
     }
-    
+
     //Called by Display when the mouse was dragged at a specific location on the pixel map
     //parameters accept the pixelMap coordinate at which the mouse started dragging and stopped dragging
     public void mouseDragged(int x1, int y1, int x2, int y2){
@@ -231,5 +253,5 @@ public class GameWorld
         draggedY2 = y2;
         mouseWasDragged = true;
     }
-      
+
 }
